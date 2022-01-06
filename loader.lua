de0e489ca888644463d58c8f155ae2e7b74222a4
ziavo <miguelgonell02@gmail.com>
2022-01-06 16:56:58 -0500

    games folder

---
+++ loader.lua
@@ -0,0 +1,18 @@
+local owner = "ziavo"
+local repository = "lego-hub"
+local branch = "main"
+
+local games = {
+
+}
+
+local function import(file)
+    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/%s/%s/%s.lua"):format(owner, repository, branch, file)))()
+end
+local function init()
+	local file_name = games[game.PlaceId]
+	if file_name then
+		import("files/"..file_name)
+	end
+end
+init()
