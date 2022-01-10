6a1e6296541df17d9d48082d71814352cb6acd44
ziavo <miguelgonell02@gmail.com>
2022-01-10 17:30:59 -0500

    Update smoothie_simulator.lua

--- games/smoothie_simulator.lua
+++ games/smoothie_simulator.lua
@@ -15,42 +15,6 @@ local debris_folder = workspace.Debris
 local fruits_folder = debris_folder.Fruit
 local player_base
 ---------------------------------------------------------------
-local function return_base(target)
-    for i,v in pairs(plots_folder:GetChildren()) do
-        if v.Info.Owner.Value == clien then return v end
-    end
-end
-
-local function add_to_blender()
-    for i,v in pairs(player_base.Blenders:GetChildren()) do
-        gameplay_folder.AddFruitToBlender:FireServer()
-    end
-end
-local handler = {
-    ["auto_grab"] = function()
-        for i,v in pairs(fruits_folder:GetChildren()) do
-            if v.Plot.Value == player_base then
-                gameplay_folder.GrabFruit:FireServer(v)
-            end
-        end
-    end,
-}
-
-shared._ids = {}
-local function bind_render(name, callback)
-    if shared._ids then
-        if shared._ids[name] and not shared.impulse[name] then
-            pcall(runService.UnbindFromRenderStep, runService, shared._ids[name])
-            shared._ids[name] = nil
-            return
-        end
-
-        if not shared.impulse[name] then return end
-        shared._ids[name] = httpService:GenerateGUID(false)
-        runService:BindToRenderStep(shared._ids[name], 1, callback)
-    end
-end
----------------------------------------------------------------
 -- Unloads Old Shit
 if shared._unload then
     pcall(shared._unload)
@@ -70,12 +34,46 @@ function shared._unload()
     end
 end
 ---------------------------------------------------------------
+local function return_base(target)
+    for i,v in pairs(plots_folder:GetChildren()) do
+        if v.Info.Owner.Value == client then return v end
+    end
+end
+
+local function add_to_blender()
+    for i,v in pairs(player_base.Blenders:GetChildren()) do
+        gameplay_folder.AddFruitToBlender:FireServer()
+    end
+end
+local function auto_grab()
+    for i,v in pairs(fruits_folder:GetChildren()) do
+        if v.Plot.Value == player_base then
+            gameplay_folder.GrabFruit:FireServer(v)
+        end
+    end
+end
+
+shared._ids = {}
+local function bind_render(name, callback)
+    if shared._ids then
+        if shared._ids[name] and not shared.impulse.features[name] then
+            pcall(runService.UnbindFromRenderStep, runService, shared._ids[name])
+            shared._ids[name] = nil
+            return
+        end
+
+        if not shared.impulse.features[name] then return end
+        shared._ids[name] = httpService:GenerateGUID(false)
+        runService:BindToRenderStep(shared._ids[name], 1, callback)
+    end
+end
+---------------------------------------------------------------
 -- UI Lib
 local library = loadstring(game:HttpGet('https://lindseyhost.com/UI/LinoriaLib.lua'))()
 
 local fonts = {}
 for font, _ in next, Drawing.Fonts do
-	table.insert(fonts, font)
+    table.insert(fonts, font)
 end;
 local windows = {
     main = library:CreateWindow("Impulse | v0.01"),
@@ -90,16 +88,20 @@ local sections = {
 local feature_names = {
     ["auto_grab"] = "Auto Grab Fruits"
 }
+
+shared.impulse = {
+    ["features"] = {
+        ["auto_grab"] = false
+    }
+}
 ---------------------------------------------------------------
 player_base = return_base(client)
 task.spawn(function() -- auto selection
-    for i,v in pairs(shared.impulse.features.auto) do
-        sections.auto:AddToggle(i, {Text = feature_names[i], Default = v,}):OnChanged(function()
-            shared.impulse.auto[i] = Toggles[i].Value
+    sections.auto:AddToggle("AutoGrab", {Text = "Auto Grab Fruits", Default = false}):OnChanged(function()
+        shared.impulse.features.auto_grab = Toggles.AutoGrab.Value
     
-            bind_render(i, handler[i])
-        end)
-    end
+        bind_render("auto_grab", auto_grab)
+    end)
 end)
 
 library:Notify(math.floor((os.clock() - start_time) * 1000) .. "ms to initialize!")
